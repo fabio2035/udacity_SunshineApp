2fc99adca01d7b56af599f3a36eade4381697666
fabio2035 <fabio2035@gmail.com>
2016-09-21 00:45:38 +0200

    Initial commit

---
+++ app/src/main/java/com/fabio/monster/sunshine/app/MainActivity.java
@@ -0,0 +1,68 @@
+package com.fabio.monster.sunshine.app;
+
+import android.support.v4.app.Fragment;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.concurrent.Callable;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public static class PlaceholderFragment extends Fragment {
+
+        public PlaceholderFragment() {
+        }
+
+        @Override
+        public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                                 Bundle savedInstanceState) {
+
+            // Create some dummy data for the ListView.  Here's a sample weekly forecast
+            String[] data = {
+                    "Mon 6/23 - Sunny - 31/17",
+                    "Tue 6/24 - Foggy - 21/8",
+                    "Wed 6/25 - Cloudy - 22/17",
+                    "Thurs 6/26 - Rainy - 18/11",
+                    "Fri 6/27 - Foggy - 21/10",
+                    "Sat 6/28 - TRAPPED IN WEATHERSTATION - 23/18",
+                    "Sun 6/29 - Sunny - 20/7"
+            };
+            List<String> weekForecast = new ArrayList<String>(Arrays.asList(data));
+
+            ArrayAdapter<String> adapter = new ArrayAdapter<String>(getActivity(),
+                    android.R.layout.list_item_forecast,
+                    android.R.layout.list_item_forecast_textview,
+                    weekForecast);
+
+            int i = 0;
+
+            
+
+
+            //Funnel the viewGroup of the child view we're looking for
+            View rootView = inflater.inflate(R.layout.fragment_main, container, false);
+
+            //Use the funneled viewGroup to find the particular view we're looking for
+            ListView listView = (ListView) rootView.findViewById(R.listView_forecast);
+
+            listView.setAdapter(adapter);
+
+            return rootView;
+        }
+    }
+}
+
